dfdb2dcd9778d891f523ef13fd821bf9dcc97017
Thomas Basset <thomas@basset.rocks>
2023-02-22 13:55:33 +0100

    WebFlux Controller Advice

--- src/main/java/rocks/basset/spring5mongorecipe/controllers/RecipeController.java
+++ src/main/java/rocks/basset/spring5mongorecipe/controllers/RecipeController.java
@@ -24,6 +24,13 @@ public class RecipeController {
     private static final String RECIPE_RECIPEFORM_URL = "recipe/recipeform";
     private final RecipeService recipeService;
 
+    private WebDataBinder webDataBinder;
+
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder){
+        this.webDataBinder = webDataBinder;
+    }
+
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
@@ -50,7 +57,10 @@ public class RecipeController {
     }
 
     @PostMapping("recipe")
-    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult){
+    public String saveOrUpdate(@ModelAttribute("recipe") RecipeCommand command){
+
+        webDataBinder.validate();
+        BindingResult bindingResult = webDataBinder.getBindingResult();
 
         if(bindingResult.hasErrors()){
 
