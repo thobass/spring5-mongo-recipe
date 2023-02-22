e6a790ddb52af1e5778b224004984c16d8339fe7
Thomas Basset <thomas@basset.rocks>
2023-02-22 08:31:37 +0100

    Convertion vers WebFlux

--- src/main/java/rocks/basset/spring5mongorecipe/controllers/ImageController.java
+++ src/main/java/rocks/basset/spring5mongorecipe/controllers/ImageController.java
@@ -3,7 +3,6 @@ package rocks.basset.spring5mongorecipe.controllers;
 import rocks.basset.spring5mongorecipe.commands.RecipeCommand;
 import rocks.basset.spring5mongorecipe.services.ImageService;
 import rocks.basset.spring5mongorecipe.services.RecipeService;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -46,21 +44,21 @@ public class ImageController {
         return "redirect:/recipe/" + id + "/show";
     }
 
-    @GetMapping("recipe/{id}/recipeimage")
-    public void renderImageFromDB(@PathVariable String id, HttpServletResponse response) throws IOException {
-        RecipeCommand recipeCommand = recipeService.findCommandById(id).block();
-
-        if (recipeCommand.getImage() != null) {
-            byte[] byteArray = new byte[recipeCommand.getImage().length];
-            int i = 0;
-
-            for (Byte wrappedByte : recipeCommand.getImage()){
-                byteArray[i++] = wrappedByte; //auto unboxing
-            }
-
-            response.setContentType("image/jpeg");
-            InputStream is = new ByteArrayInputStream(byteArray);
-            IOUtils.copy(is, response.getOutputStream());
-        }
-    }
+//    @GetMapping("recipe/{id}/recipeimage")
+//    public void renderImageFromDB(@PathVariable String id, HttpServletResponse response) throws IOException {
+//        RecipeCommand recipeCommand = recipeService.findCommandById(id).block();
+//
+//        if (recipeCommand.getImage() != null) {
+//            byte[] byteArray = new byte[recipeCommand.getImage().length];
+//            int i = 0;
+//
+//            for (Byte wrappedByte : recipeCommand.getImage()){
+//                byteArray[i++] = wrappedByte; //auto unboxing
+//            }
+//
+//            response.setContentType("image/jpeg");
+//            InputStream is = new ByteArrayInputStream(byteArray);
+//            IOUtils.copy(is, response.getOutputStream());
+//        }
+//    }
 }
